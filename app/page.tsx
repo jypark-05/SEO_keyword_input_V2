86a62de06befb9c5bb7f4d198de049e2fb69aabc
jypark-05 <parkjy521@gmail.com>
2026-04-08 19:17:34 +0900

    수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useEffect, useState, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import ProgressBar from "@/components/ProgressBar";
